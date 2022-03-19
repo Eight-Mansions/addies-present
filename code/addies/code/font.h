1e5237786d366134a32cd6ddcb6b6520f222ceac
esperknight <esperknight@yahoo.com>
2022-03-18 20:57:05 -0400

    Checking in missing files

---
+++ code/addies/code/font.h
@@ -0,0 +1,15 @@
+#ifndef FONT_H_
+#define FONT_H_
+
+#include "platform.h"
+
+extern "C" {
+
+	int GetLetterWidth(u32 letter);
+
+	int GetSentenceWidth(const char* text, int length);
+
+	int GetYForCentering(const char* text, u32 length);
+}
+
+#endif
