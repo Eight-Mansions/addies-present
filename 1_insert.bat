6c192c00fcc6bf2392b3ff8ee9c9f906242ccc35
esperknight <esperknight@yahoo.com>
2023-09-17 22:43:31 -0400

    Updated insertion batch file to call the assembler files for each area

--- 1_insert.bat
+++ 1_insert.bat
@@ -2,4 +2,24 @@ del error.txt
 del /q /s ins\*
 xcopy /s orig\* ins
 
+tools\armips.exe code\CHAP1\WORLD1.DLR.asm
+tools\armips.exe code\CHAP1\WORLD2.DLR.asm
+tools\armips.exe code\CHAP1\WORLD3.DLR.asm
+tools\armips.exe code\CHAP1\WORLD4.DLR.asm
+tools\armips.exe code\CHAP1\WORLD5.DLR.asm
+tools\armips.exe code\CHAP2\WORLD1.DLR.asm
+tools\armips.exe code\CHAP2\WORLD2.DLR.asm
+tools\armips.exe code\CHAP2\WORLD3.DLR.asm
+tools\armips.exe code\CHAP2\WORLD4.DLR.asm
+tools\armips.exe code\CHAP2\WORLD5.DLR.asm
+tools\armips.exe code\CHAP2\WORLD6.DLR.asm
+tools\armips.exe code\CHAP2\WORLD7.DLR.asm
+tools\armips.exe code\CHAP2\WORLD8.DLR.asm
+tools\armips.exe code\CHAP2\WORLD9.DLR.asm
+tools\armips.exe code\CHAP3\WORLD1.DLR.asm
+tools\armips.exe code\CHAP3\WORLD2.DLR.asm
+tools\armips.exe code\CHAP3\WORLD3.DLR.asm
+tools\armips.exe code\CHAP3\WORLD4.DLR.asm
+tools\armips.exe code\CHAP3\WORLD5.DLR.asm
+
 tools\addies_present_text_insertion.exe trans sjis.tbl
---
+++ code/CHAP1/WORLD4.DLR.asm
@@ -0,0 +1,4 @@
+.psx
+.open "ins\CHAP1\WORLD4.DLR",0x800B7B78
+
+.close
