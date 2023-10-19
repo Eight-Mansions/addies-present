776c735965411a943abfde31761e2d7dafce5c8e
esperknight <esperknight@yahoo.com>
2023-10-18 23:38:09 -0400

    Fixed save names for Zoo's house and Conductor's Hall

--- code/CHAP1/WORLD4.DLR.asm
+++ code/CHAP1/WORLD4.DLR.asm
@@ -10,5 +10,16 @@
 	;封印された扉の外には\nとても広い野原が広がっていた。
 	;Beyond the locked door\nwas a wide field.
 	addiu v0, a0, 0x30
+	
+.org 0x800bcc50
+	;管理人室・ハシゴ
+	;Conductor's Hall
+    lw t4, 0xc(t6)
+    sw t3, 0x8(t5)
+    sw t4, 0xc(t5)
+    lw t3, 0x10(t6)
+    nop
+    sw t3, 0x10(t5)
+
 
 .close
--- code/CHAP1/WORLD5.DLR.asm
+++ code/CHAP1/WORLD5.DLR.asm
@@ -16,5 +16,22 @@
 	;Joka waited for Addie\nin the music box core.
 	addiu v0, a0, 0x30
 
+.org 0x800bd32c
+	;管理人室・ハシゴ
+	;Conductor's Hall
+	lw t3, 0x10(t6)
+    nop
+    sw t3, 0x10(t5)
+
+.org 0x800bcc50
+	;管理人室・ハシゴ
+	;Conductor's Hall
+	lw t2, 0xc(t4)
+    sw t1, 0x8(t3)
+    sw t2, 0xc(t3)
+    lw t1, 0x10(t4)
+    nop
+    sw t1, 0x10(t3)
+
 
 .close
